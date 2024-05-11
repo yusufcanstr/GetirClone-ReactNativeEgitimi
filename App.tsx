12c9449052bb4a66e45769c26f634b5e8401727c
Yusufcan Senturk <developer.yusufcan@gmail.com>
2024-05-11 14:24:10 +0300

    React Native Getir Clone #2 - Ana Sayfayı Oluşturup Adres Component ile Başlayalım

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from "./src/screens/HomeScreen"
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <HomeScreen />
     </View>
   );
 }
@@ -13,8 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'white',
+    marginTop: "15%",
   },
 });
