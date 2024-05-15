1562af0a6616fd1ed87bbbaf69feea35dd4d7e9a
Yusufcan Senturk <developer.yusufcan@gmail.com>
2024-05-15 23:53:55 +0300

    Sepet Ekranı Sepet Item List ve Devam Et Button Componentleri Oluşturma

--- src/components/CartItem/index.tsx
+++ src/components/CartItem/index.tsx
@@ -9,46 +9,62 @@ export default function index({ product }: CartItemProps) {
   return (
     <View
       style={{
-        height: height * 0.13,
-        paddingHorizontal: width * 0.04,
+        width: "100%",
         backgroundColor: "white",
         flexDirection: "row",
         alignItems: "center",
         justifyContent: "space-between",
       }}
     >
-      <View style={{ flexDirection: "row", alignItems: "center" }}>
-        <Image
-          style={{ height: height * 0.09, width: height * 0.09 }}
-          source={{ uri: product.image }}
-        />
-        <View style={{ marginLeft: 8 }}>
-          <Text style={styles.txtName}>{product.name}</Text>
-          <Text style={styles.txtMiktar}>{product.miktar}</Text>
-          <Text style={styles.txtFiyat}>₺{product.fiyat}</Text>
-        </View>
-      </View>
-
       <View
         style={{
+          height: height * 0.13,
+          width: width * 0.92,
+          marginHorizontal: width * 0.04,
+          flex: 1,
+          backgroundColor: "white",
           flexDirection: "row",
-          justifyContent:"space-around",
-          alignItems:"center",
-          width: width * 0.21,
-          borderColor: "lightgray",
-          borderWidth: 0.5,
-          height: height * 0.037,
-          borderRadius:10,
+          justifyContent: "space-between",
+          alignItems: "center",
+          borderBottomWidth: 0.4,
+          borderBottomColor: "lightgrey",
         }}
       >
-        <View style={{flex:1, alignItems:"center"}}>
-          <Text>-</Text>
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <Image
+            style={{ height: height * 0.09, width: height * 0.09 }}
+            source={{ uri: product.image }}
+          />
+          <View style={{ marginLeft: 8 }}>
+            <Text style={styles.txtName}>{product.name}</Text>
+            <Text style={styles.txtMiktar}>{product.miktar}</Text>
+            <Text style={styles.txtFiyat}>₺{product.fiyat}</Text>
+          </View>
         </View>
-        <View style={{flex:1, alignItems:"center", backgroundColor:"#5D3EBD", height:height*0.037, justifyContent:"center"}}>
-          <Text style={{fontWeight:"bold", color:"white", fontSize:12}}>2</Text>
-        </View>
-        <View style={{flex:1, alignItems:"center"}}>
-          <Text>+</Text>
+
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-around",
+            alignItems: "center",
+            width: width * 0.21,
+            borderColor: "lightgray",
+            borderWidth: 0.5,
+            height: height * 0.037,
+            borderRadius: 10,
+            shadowOpacity: 0.6,
+            shadowColor: "gray",
+          }}
+        >
+          <View style={{ flex: 1, alignItems: "center" }}>
+            <Text>-</Text>
+          </View>
+          <View style={{ flex: 1, alignItems: "center", backgroundColor: "#5D3EBD", height: height * 0.037, justifyContent: "center" }}>
+            <Text style={{ fontWeight: "bold", color: "white", fontSize: 12 }}>2</Text>
+          </View>
+          <View style={{ flex: 1, alignItems: "center" }}>
+            <Text>+</Text>
+          </View>
         </View>
       </View>
     </View>
@@ -59,6 +75,7 @@ const styles = StyleSheet.create({
   txtName: {
     fontSize: 13,
     fontWeight: "600",
+    maxWidth: width * 0.46,
   },
   txtMiktar: {
     fontSize: 12,
--- src/screens/CartScreen/index.tsx
+++ src/screens/CartScreen/index.tsx
@@ -1,14 +1,77 @@
-import { StyleSheet, Text, View, FlatList } from 'react-native'
-import React from 'react'
-import productsGetir from '../../../assets/productsGetir'
-import CartItem from '../../components/CartItem'
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  Dimensions,
+  TouchableOpacity,
+} from "react-native";
+import React from "react";
+import productsGetir from "../../../assets/productsGetir";
+import CartItem from "../../components/CartItem";
 
+const { width, height } = Dimensions.get("window");
 export default function index() {
   return (
-    <FlatList
-    data={productsGetir.slice(2,3)}
-    renderItem={({item}) => <CartItem product={item}/>}/>
-  )
+    <View style={{ flex: 1 }}>
+      <FlatList
+        data={productsGetir.slice(0, 4)}
+        renderItem={({ item }) => <CartItem product={item} />}
+      />
+      <View
+        style={{
+          height: height * 0.12,
+          flexDirection: "row",
+          alignItems: "center",
+          paddingHorizontal: "4%",
+          backgroundColor: "#f9f9f9",
+        }}
+      >
+        <TouchableOpacity
+          style={{
+            flex: 3,
+            borderBottomLeftRadius: 8,
+            borderTopLeftRadius: 8,
+            backgroundColor: "#5D3EBD",
+            height: height * 0.06,
+            justifyContent: "center",
+            alignItems: "center",
+            marginTop: -10,
+          }}
+        >
+          <Text style={{ color: "white", fontSize: 15, fontWeight: "bold" }}>
+            Devam
+          </Text>
+        </TouchableOpacity>
+
+        <View
+          style={{
+            flex: 1,
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: "white",
+            height: height * 0.06,
+            marginTop:-10,
+            borderTopRightRadius:8,
+            borderBottomRightRadius:8,
+            borderWidth:0.5,
+            borderColor:"lightgray"
+          }}
+        >
+          <Text
+            style={{
+              color: "#5D3EBD",
+              fontWeight: "bold",
+              fontSize: 15,
+            }}
+          >
+            <Text>{"\u20BA"}</Text>24.95
+          </Text>
+        </View>
+
+      </View>
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
